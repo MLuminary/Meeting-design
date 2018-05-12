b2b0bfee6f0590ff9b56033559e10dae99a664b0
MLuminary <418037025@qq.com>
2018-05-12 16:34:06 +0800

    嘉宾页完成

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@
   //第一屏跟随滑动视角
   $(window).scroll(function(event){
     var top = $(document).scrollTop()
-    console.log(top)
-    $('.ht_bg_box').css('transform',`translate(0px,${top}px)`)
+    if(top < 1000){
+      $('.ht_bg_box').css('transform',`translate(0px,${top}px)`)
+    }
   })
 })()
