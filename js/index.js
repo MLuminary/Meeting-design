95968f0135f64d71e2fb0c102723e63ae3bd005f
MLuminary <418037025@qq.com>
2018-05-13 15:59:26 +0800

    首页动态完成

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
 ;(function(){
-  //第一屏跟随滑动视角
+  //首页直接展示
+  $('.ht_section_0_info').addClass('show');
+
   $(window).scroll(function(event){
+      //第一屏跟随滑动视角
     var top = $(document).scrollTop()
     if(top < 1000){
       $('.ht_bg_box').css('transform',`translate(0px,${top}px)`)
+    }
+
+    console.log(top)
+    //第二屏文字延时出现
+    if( top >$('.ht_section_0').height()/2){
+      $('.ht_section_1 .row').addClass('show');
+    }
+
+    if(top >$('.ht_section_2').offset().top){
+      $('.ht_section_3 .row img').addClass('show');
+    }
+
+    if(top > $('.ht_section_3').offset().top){
+      $('.ht_section_4 .row').addClass('show')
+    }
+    if(top > $('.ht_section_4').offset().top){
+      $('.ht_section_5 .row').addClass('show')
     }
   })
 })()
